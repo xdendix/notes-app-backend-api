2800603bd415ea743fb530b31b714f21f7bae4bb
xdendix <internaldendi@gmail.com>
2023-01-13 03:20:19 +0700

    postgres database implemented

---
+++ migrations/1673518042848_create-table-notes.js
@@ -0,0 +1,36 @@
+/* eslint-disable camelcase */
+
+const notes = require('../src/api/notes');
+
+exports.up = (pgm) => {
+  pgm.createTable('notes', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    tags: {
+      type: 'TEXT[]',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable(notes);
+};
